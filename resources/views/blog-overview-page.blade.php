27e13b361236ba7b0f5732fbbe3931576460f1d7
Leon Kuijf <leon.kuijf@gmail.com>
2023-09-14 11:25:55 +0200

    blog cards styled

--- resources/views/blog-overview-page.blade.php
+++ resources/views/blog-overview-page.blade.php
@@ -8,13 +8,13 @@
     @foreach ($data['blog_items'] as $blogItem)
         <article>
             @if (isset($blogItem->gallery[0]) && isset($blogItem->gallery[0]['sizes']))
-                <img src="{{ $blogItem->gallery[0]['sizes']['medium'] }}" alt="{{ $blogItem->gallery[0]['alt'] }}">
+                <img src="{{ $blogItem->gallery[0]['sizes']['medium_large'] }}" alt="{{ $blogItem->gallery[0]['alt'] }}">
             @else
                 <img src="{{ $blogItem->gallery[0]['url'] }}" alt="{{ $blogItem->gallery[0]['alt'] }}">
             @endif
             <div>
+                <p class="date">{{ date('d-m', strtotime($blogItem->date)) }}</p>
                 <h2>{{ $blogItem->title }}</h2>
-                <p class="date">{{ date('d-m-Y', strtotime($blogItem->date)) }}</p>
                 <p>{!! $blogItem->card_text !!}</p>
                 <p><a href="{{ url('blog/' . $blogItem->slug) }}">Lees verder ></a></p>
             </div>
