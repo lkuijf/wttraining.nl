d943f8b49d1d3e6c2517a807554b596ea0f8701a
Leon Kuijf <leon.kuijf@gmail.com>
2023-09-20 08:50:47 +0200

    add and remove posts

--- public/_mcfu638b-cms/wp-content/themes/wtcustom/functions.php
+++ public/_mcfu638b-cms/wp-content/themes/wtcustom/functions.php
@@ -9,7 +9,7 @@ require('inc/callbacks.inc.php');
 // require('inc/filter-job-offers.inc.php');
 
 $editorCanAddAndRemovePages = false; // !!! may take 2 reloads for changes to take effect !!!
-$editorCanAddAndRemovePosts = false; // !!! may take 2 reloads for changes to take effect !!!
+$editorCanAddAndRemovePosts = true; // !!! may take 2 reloads for changes to take effect !!!
 
 $carbonFieldsArgs = array();
 $websiteOptions = array();
