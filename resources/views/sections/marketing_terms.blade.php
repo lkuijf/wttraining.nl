da357cc37332e18aee6268c01e02f87f3d6b9579
Leon Kuijf <leon.kuijf@gmail.com>
2023-09-20 14:24:41 +0200

    2 less marketing terms

--- resources/views/sections/marketing_terms.blade.php
+++ resources/views/sections/marketing_terms.blade.php
@@ -7,8 +7,8 @@
             @if($image4) @include('sections.marketingTerm', ['image' => $image4, 'title' => $term4, 'text' => $term4_text, 'color' => '#232F70']) @endif
             @if($image5) @include('sections.marketingTerm', ['image' => $image5, 'title' => $term5, 'text' => $term5_text, 'color' => '#3F034A']) @endif
             @if($image6) @include('sections.marketingTerm', ['image' => $image6, 'title' => $term6, 'text' => $term6_text, 'color' => '#232F70']) @endif
-            @if($image7) @include('sections.marketingTerm', ['image' => $image7, 'title' => $term7, 'text' => $term7_text, 'color' => '#5B0808']) @endif
-            @if($image8) @include('sections.marketingTerm', ['image' => $image8, 'title' => $term8, 'text' => $term8_text, 'color' => '#246D57']) @endif
+            {{-- @if($image7) @include('sections.marketingTerm', ['image' => $image7, 'title' => $term7, 'text' => $term7_text, 'color' => '#5B0808']) @endif
+            @if($image8) @include('sections.marketingTerm', ['image' => $image8, 'title' => $term8, 'text' => $term8_text, 'color' => '#246D57']) @endif --}}
         </div>
     </div>
 </div>
