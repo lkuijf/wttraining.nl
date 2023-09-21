68d2f017927316574641ac11ff28a88d8b7123c8
Leon Kuijf <leon.kuijf@gmail.com>
2023-09-21 12:31:57 +0200

    removed trailing slash

--- resources/views/sections/video.blade.php
+++ resources/views/sections/video.blade.php
@@ -9,7 +9,7 @@ height="264"
 {{-- poster="{{ asset('statics/wtt-logo.png') }}" --}}
 data-setup="{}"
 >
-<source src="{{ $videoUrl }}" type="video/mp4" />
+<source src="{{ $videoUrl }}" type="video/mp4">
 {{-- <source src="MY_VIDEO.webm" type="video/webm" /> --}}
 <p class="vjs-no-js">
   To view this video please enable JavaScript, and consider upgrading to a
