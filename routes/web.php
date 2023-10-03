d8d8b03e06f06fd8d570e8aa21d7243697f3842c
Leon Kuijf <leon.kuijf@gmail.com>
2023-10-03 19:36:23 +0200

    nicer routes file

--- routes/web.php
+++ routes/web.php
@@ -16,14 +16,21 @@ use Spatie\ResponseCache\Facades\ResponseCache;
 | contains the "web" middleware group. Now create something great!
 |
 */
-/*** Moved some web.config rewrites/redirects to these routes. Since Laravel toolkit, and others, for Plesk messes web.config up) ******************/
+
+/*** Moved some web.config rewrites/redirects to these routes. Since Laravel toolkit, and others, for Plesk messes web.config up) ************************************/
 Route::get('/media/{year}/{month}/{file}', [ImageController::class, 'renderImage'])->where(['year' => '[0-9]{4}','month' => '[0-9]{2}']); // rewrite for media
 Route::get('/wp-json/carbon-fields/v1/attachment', function () {return redirect(str_replace('/wp-json/carbon-fields/v1/attachment', '/_mcfu638b-cms/index.php/wp-json/carbon-fields/v1/attachment', Request::fullUrl()));}); // redirect for Carbon Fields -bug
 Route::get('/wp-json/carbon-fields/v1/association', function () {return redirect(str_replace('/wp-json/carbon-fields/v1/association', '/_mcfu638b-cms/index.php/wp-json/carbon-fields/v1/association', Request::fullUrl()));}); // redirect for Carbon Fields -bug
 Route::get('/wp-json/carbon-fields/v1/association/options', function () {return redirect(str_replace('/wp-json/carbon-fields/v1/association/options', '/_mcfu638b-cms/index.php/wp-json/carbon-fields/v1/association/options', Request::fullUrl()));}); // redirect for Carbon Fields -bug
 Route::get('/admin', function () {return redirect('/_mcfu638b-cms/wp-admin');}); // redirect /admin to wp-cms
-/*** TIP: Check the Web Application Firewall when a route is not picked up by a rule above *********************************************************/
-/***************************************************************************************************************************************************/
+// --- TIP: Check the Web Application Firewall when a route is not picked up by a rule above ---
+
+/*** Routes for some custom WP-API endpoints, so they can be cached by Laravel Response Cache ************************************************************************/
+// --- Speeds up loading time of pages on initial call (except for the very first!) when all Response Caches are flushed. Since these endpoints are used by all pages.
+Route::get('/wtcustom/simple-pages', function () {return file_get_contents(config('app_wt.host') . config('app_wt.cmsPath') . '/index.php/wp-json/wtcustom/simple-pages');});
+Route::get('/wtcustom/website-options', function () {return file_get_contents(config('app_wt.host') . config('app_wt.cmsPath') . '/index.php/wp-json/wtcustom/website-options');});
+Route::get('/wtcustom/simple-media', function () {return file_get_contents(config('app_wt.host') . config('app_wt.cmsPath') . '/index.php/wp-json/wtcustom/simple-media');});
+/*********************************************************************************************************************************************************************/
 
 Route::get('/', function () {
     // return view('welcome');
@@ -36,12 +43,6 @@ Route::get('/clear-response-cache-wt', function () {
     ResponseCache::clear();
     return 'Response Cache Cleared!';
 })->middleware('doNotCacheResponse');
-
-
-Route::get('/wtcustom/simple-pages', function () {return file_get_contents(config('app_wt.host') . config('app_wt.cmsPath') . '/index.php/wp-json/wtcustom/simple-pages');});
-Route::get('/wtcustom/website-options', function () {return file_get_contents(config('app_wt.host') . config('app_wt.cmsPath') . '/index.php/wp-json/wtcustom/website-options');});
-Route::get('/wtcustom/simple-media', function () {return file_get_contents(config('app_wt.host') . config('app_wt.cmsPath') . '/index.php/wp-json/wtcustom/simple-media');});
-
 
 
 Route::post('/submit-subscription-form', [SubmitController::class, 'submitSubscriptionFormXHR']);
