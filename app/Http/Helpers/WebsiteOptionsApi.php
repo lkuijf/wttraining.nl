18431fe63e47601bc046909664c49fb75c6ac3d7
Leon Kuijf <leon.kuijf@gmail.com>
2023-10-03 18:55:14 +0200

    testing if it finaly works

--- app/Http/Helpers/WebsiteOptionsApi.php
+++ app/Http/Helpers/WebsiteOptionsApi.php
@@ -3,7 +3,7 @@ namespace App\Http\Helpers;
 
 class WebsiteOptionsApi extends ApiCall {
     public function __construct() {
-        $this->endpoint = config('app_wt.cmsPath')  . '/index.php/wp-json/wtcustom/website-options';
-        // $this->endpoint = '/wtcustom/website-options';
+        // $this->endpoint = config('app_wt.cmsPath')  . '/index.php/wp-json/wtcustom/website-options';
+        $this->endpoint = '/wtcustom/website-options';
     }
 }
