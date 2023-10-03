71ba38f41f762ba50022bdac2bbce193af4982d2
Leon Kuijf <leon.kuijf@gmail.com>
2023-10-03 18:52:16 +0200

    added host for images

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class ImageController extends Controller
 {
     public function renderImage(Request $request, $year, $month, $file) {
-        $urlToWpImage = config('app_wt.cmsPath') . '/wp-content/uploads/' . $year . '/' . $month . '/' . $file;
+        $urlToWpImage = config('app_wt.host') . config('app_wt.cmsPath') . '/wp-content/uploads/' . $year . '/' . $month . '/' . $file;
         $placeholder = 'https://placehold.jp/24/ccc/000/250x250.png?text=Geen+afbeelding+gevonden';
 
         $imgSize = getimagesize($urlToWpImage);
