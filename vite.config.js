f0a358224152290dd5fd27dba55d79cba644a22e
Leon Kuijf <leon.kuijf@gmail.com>
2023-10-05 16:32:22 +0200

    added Asset Bundling (Vite)

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,16 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                // 'resources/css/app.css',
+                'resources/scss/styles.scss',
+                'resources/css/swiper-bundle.min.css',
+                'resources/css/video-js.css',
+                // 'resources/js/app.js',
+                'resources/js/swiper-bundle.min.js',
+                'resources/js/script.js',
+            ],
+            buildDirectory: 'bundle',
             refresh: true,
         }),
     ],
