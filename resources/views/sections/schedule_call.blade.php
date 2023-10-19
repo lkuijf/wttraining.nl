9f757c8775a2a9fc3fa818daac0297de47122601
Leon Kuijf <leon.kuijf@gmail.com>
2023-10-19 17:11:19 +0200

    rounded up

--- resources/views/sections/schedule_call.blade.php
+++ resources/views/sections/schedule_call.blade.php
@@ -11,19 +11,19 @@
                         <input type="hidden" name="success_text" value="{{ $success_text }}">
                         <input type="hidden" name="email_to" value="{{ $email_to }}">
                         <div class="formRow">
-                            <div><img src="{{ asset('statics/contact-user.png') }}" alt="naam"></div>
+                            <div><img src="{{ asset('statics/contact-user.png') }}" alt="naam" class="noshadow"></div>
                             <div @error('name')class="error" data-err-msg="{{ $message }}"@enderror><input type="text" name="name" value="{{ old('name') }}" placeholder="Je naam"></div>
                         </div>
                         <div class="formRow">
-                            <div><img src="{{ asset('statics/contact-email.png') }}" alt="naam"></div>
+                            <div><img src="{{ asset('statics/contact-email.png') }}" alt="email" class="noshadow"></div>
                             <div @error('email')class="error" data-err-msg="{{ $message }}"@enderror><input type="text" name="email" value="{{ old('email') }}" placeholder="Je e-mail adres"></div>
                         </div>
                         <div class="formRow">
-                            <div><img src="{{ asset('statics/contact-company.png') }}" alt="naam"></div>
+                            <div><img src="{{ asset('statics/contact-company.png') }}" alt="company" class="noshadow"></div>
                             <div><input type="text" name="company" value="{{ old('company') }}" placeholder="Bedrijfsnaam"></div>
                         </div>
                         <div class="formRow">
-                            <div><img src="{{ asset('statics/contact-phone.png') }}" alt="naam"></div>
+                            <div><img src="{{ asset('statics/contact-phone.png') }}" alt="message" class="noshadow"></div>
                             <div @error('phone')class="error" data-err-msg="{{ $message }}"@enderror><input type="text" name="phone" value="{{ old('phone') }}" placeholder="Je telefoonnummer"></div>
                         </div>
                         {{-- <div><textarea name="message" cols="30" rows="3" value="{{ old('message') }}" placeholder="Reden van contact"></textarea></div> --}}
