39b1687c2a9435ee77f9ef7c6f36c7687862a902
Leon Kuijf <leon.kuijf@gmail.com>
2023-10-19 14:57:43 +0200

    enabled video js

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,9 +2,9 @@
 
 // import Swiper JS
 import Swiper from 'swiper/bundle';
-// import videojs from 'video.js/dist/video.js';
+import videojs from 'video.js/dist/video.js';
 // import Swiper styles
 import 'swiper/css/bundle';
-// import 'video.js/src/css/video-js.scss';
+import 'video.js/src/css/video-js.scss';
 window.Swiper = Swiper; // Prevents removal from tree-shaking. Vite workaround. When function is called from a blade template, within <script> -tag
-//window.videojs = videojs; // Prevents removal from tree-shaking. Vite workaround. When function is called from a blade template, within <script> -tag
+window.videojs = videojs; // Prevents removal from tree-shaking. Vite workaround. When function is called from a blade template, within <script> -tag
